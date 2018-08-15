083902811e379fe31da84b2d4d5e96af3423879a
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-08-15 14:51:06 -0600

    dragons

--- app/components/DragonduelService.js
+++ app/components/DragonduelService.js
@@ -1,8 +1,31 @@
 import Dragon from '../models/Dragon.js';
 import Champion from '../models/Champion.js'
 
+
+// @ts-ignore
+const dragonsApi = axios.create({
+    baseURL:'https://dragon-duel.herokuapp.com/api/dragons',
+    timeout: 3000
+
+})
+
 export default class DragonduelService{
     constructor(){
-
     }
+
+
+    getDragons(draw){
+        dragonsApi.get()
+        .then(res=> {
+            let dragons = res.data.data.map(rawDragon => {
+                return new Dragon(rawDragon)
+            })
+            draw(dragons)
+        })
+    }
+
+
+
+
+    
 }
