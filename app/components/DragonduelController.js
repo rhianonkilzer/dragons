03dc1ab1fb9d4b4bf6e0299d2a2db0dcaa366b29
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-08-15 16:22:10 -0600

    changes

--- app/components/DragonduelController.js
+++ app/components/DragonduelController.js
@@ -3,17 +3,36 @@ import DragonduelService from './DragonduelService.js'
 
 let dragonService = new DragonduelService()
 
+function drawChampions(champions) {
+    let template = ''
+    for (let i = 0; i < champions.length; i++) {
+        const champion = champions[i];
+        template += `
+        <div class="col-12 champions text-center">
+        <img src="${champion.imgUrl}" alt="champs">
+        <p>${champion.id}</p>
+        <p>${champion.name}</p>
+        <p>${champion.race}</p>
+        <p>${champion.hp}</p> 
+        <p>${champion.class}</p>
+        </div>
+        `
+    }
+    document.getElementById('champions').innerHTML = template
+}
+
 function drawDragons(dragons) {
     let template = ''
     for (let i = 0; i < dragons.length; i++) {
         const dragon = dragons[i];
         template += `
         <div class="col-12 dragons">
+        <img src="${dragon.imgUrl}" alt="draaaaaayginz">
         <p>${dragon.id}</p>
         <p>${dragon.name}</p>
         <p>${dragon.maxHp}</p>
         <p>${dragon.currentHp}</p> 
-        <img src="${dragon.imgUrl}" alt="draaaaaayginz">
+        
         </div>
         `
     }
@@ -23,6 +42,7 @@ function drawDragons(dragons) {
 export default class DragonduelController {
     constructor() {
         dragonService.getDragons(drawDragons)
+        dragonService.getChampions(drawChampions)
     }
-    
+
 }
--- app/components/DragonduelService.js
+++ app/components/DragonduelService.js
@@ -1,31 +1,44 @@
 import Dragon from '../models/Dragon.js';
 import Champion from '../models/Champion.js'
 
+// @ts-ignore
+const championsApi = axios.create({
+    baseURL: 'https://dragon-duel.herokuapp.com/api/champions/',
+    timeout: 3000
 
+})
 // @ts-ignore
 const dragonsApi = axios.create({
-    baseURL:'https://dragon-duel.herokuapp.com/api/dragons/',
+    baseURL: 'https://dragon-duel.herokuapp.com/api/dragons/',
     timeout: 3000
 
 })
 
-export default class DragonduelService{
-    constructor(){
+export default class DragonduelService {
+    constructor() {
     }
 
 
-    getDragons(draw){
+    getDragons(draw) {
         dragonsApi.get()
-        .then(res=> {
-            let dragons = res.data.data.map(rawDragon => {
-                return new Dragon(rawDragon)
+            .then(res => {
+                let dragons = res.data.map(rawDragon => {
+                    return new Dragon(rawDragon)
+                })
+                draw(dragons)
             })
-            draw(dragons)
+    }
+
+
+    getChampions(draw) {
+        championsApi.get()
+        .then(res=> {
+            let champions= res.data.map(rawChampion => {
+                return new Champion(rawChampion)
+            })
+            draw(champions)
         })
     }
 
 
-
-
-    
 }
