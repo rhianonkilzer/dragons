5a1679ebb7f09596d27bf3f30e8c7c658ee78b54
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-08-15 14:27:27 -0600

    models

---
+++ app/models/Champion.js
@@ -0,0 +1,14 @@
+
+    export default class Champion{
+        constructor(data){
+            this.id = data._id
+            this.name = data.name
+            this.imgUrl = data.imgUrl
+            this.race = data.race
+            this.class = data.class
+            this.hp = data.hp
+            this.attacks = data.attacks
+
+        }
+    }
+
---
+++ app/models/Dragon.js
@@ -0,0 +1,14 @@
+
+
+
+export default class Dragon{
+    constructor(data){
+        this.id = data._id
+        this.name = data.name
+        this.imgUrl = data.imgUrl
+        this.maxHp = data.maxHp
+        this.currentHp = data.currentHp
+    }
+}
+
+
